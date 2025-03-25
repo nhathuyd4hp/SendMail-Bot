26d3986706ebacabe4d9d390be9d6e1543f2f67c
nhathuyd4hp <nhathuyd4hp@gmail.com>
2025-03-25 16:58:45 +0700

    5:00 25/03/2025

--- bot/google_sheet.py
+++ bot/google_sheet.py
@@ -2,29 +2,35 @@ import pandas as pd
 import logging
 import json
 import requests
-from typing import Tuple
+from typing import Tuple,Union
+from enum import StrEnum
 
 class GoogleSheet:
     def __init__(self,application_scripts_url:str,timeout:int=10,logger_name:str=__name__,):
         self.application_scripts_url = application_scripts_url
         self.timeout = timeout
         self.logger = logging.getLogger(logger_name)
+        self.client = requests.Session()
+        self.client.headers.update(
+            {"Content-Type": "application/json"},
+        )  
         
-    def getSheet(self,sheet_id) -> Tuple[str,pd.DataFrame] | None:
+    def getSheet(self,sheet_id: Union[StrEnum,str]) -> Tuple[str,pd.DataFrame] | None:
         """ 
         Args:
             sheet_id (_type_): sheet_id:any
-
         Returns:
             Tuple[str,pd.DataFrame]: sheet_name, data 
             None: None
         """
-        sheet_id = str(sheet_id)
-        url = f"{self.application_scripts_url}?gid={sheet_id}"
-        response = requests.get(
-            url = url,
-            timeout=self.timeout,
+        if isinstance(sheet_id,StrEnum):
+            sheet_id = sheet_id.value
+        
+        response = self.client.get(
+            url = f"{self.application_scripts_url}?gid={sheet_id}",
+            timeout = self.timeout,
         )
+        
         if response.status_code == 200:
             content = json.loads(response.content.decode("utf-8"))
             if "error" in content:
@@ -43,4 +49,8 @@ class GoogleSheet:
         else:
             self.logger.error("Lỗi đọc dữ liệu từ Google Sheet")
             return None
+        
+    def setColor(self,sheet_id: Union[StrEnum,str],row,color:str) -> bool:
+        self.client.post()
+        
             
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+#!.venv\Scripts\python.exe
 import pandas as pd
 from bot import GoogleSheet
 from bot import SharePoint
-from enum import Enum
+from enum import StrEnum
 import logging
 
 logging.basicConfig(
@@ -15,7 +16,7 @@ logging.basicConfig(
     ],
 )
 
-class Color(Enum):
+class Color(StrEnum):
     Blue = "#00ffff"
     Yellow = "#f1c232"
     White = "#ffffff"
@@ -23,24 +24,19 @@ class Color(Enum):
     Pink = "#ead1dc"
     Green = "#93c47d"
     
-from enum import Enum
-
-class Status(Enum):
+class Status(StrEnum):
     Completed = "#999999"  
     ProcessType1 = "#00ffff"  # Type 1
     ProcessType2 = "#93c47d"  # Type 2
     
-class SheetMapping(Enum):
+class SheetMapping(StrEnum):
     KENTEC_1_T1_T6 = "976467386"
     KENTEC_2_T1_T6 = "1303713830"
     パネル_T1_T6 = "1709675894"
     
     def __str__(self):
         return self.value
-    
-# def pre_processing(df:pd.DataFrame) -> pd.DataFrame:
-#     return df[df["ID MAIL"] != ""]
-
+   
 APP_SCRIPTS_URL = "https://script.google.com/macros/s/AKfycbzQ_W_YHoffYZc-V-AqFOayYk_RTIFgr_owPKLMzsf5pZDCUoMIH331sAHuFlLyJaVT/exec"
 TIME_OUT = 10
 
@@ -52,15 +48,16 @@ if __name__ == "__main__":
     )
     for sid in SheetMapping:
         sheet_name,sheet = google_sheet.getSheet(sid)
+        sheet.index = sheet.index + 2 # Tăng index lên 2 đơn vị (Excel bắt đầu từ 1 và Mất 1 dòng header)
         # Loại các các dòng có ID MAIL rỗng
         sheet = sheet[sheet["ID MAIL"] != ""] 
-        # Chỉ lấy các dòng là Status.ProcessType1 hoặc Status.ProcessType2
-        sheet = sheet[sheet["background"].apply(lambda bg_list: 
-            (len(bg_list) == 1 and bg_list[0] in [Status.ProcessType1.value, Status.ProcessType2.value]) 
-            or 
-            (len(bg_list) == 2 and any(color in [Status.ProcessType1.value, Status.ProcessType2.value] for color in bg_list) and Color.White.value in bg_list)
-        )]
-        sheet.to_excel(f"{sheet_name}.xlsx",index_label="STT")    
+        # Duyệt qua các MAIL_ID
+        MAIL_IDS = sheet["ID MAIL"].unique()
+        for mail_id in MAIL_IDS:
+            sheet_mail = sheet[sheet["ID MAIL"] == mail_id]
+            bgs = sheet_mail['background'].to_list()
+            
+        
     # sharepoint = SharePoint(
     #     username="vietnamrpa@nskkogyo.onmicrosoft.com",
     #     password="Robot159753",
